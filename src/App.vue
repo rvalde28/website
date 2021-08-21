38d86de09e6fef78838fe2ca40faced5bb28e18d
Ruben Valdez Luna <ruben.valdez87@gmail.com>
2021-08-21 15:55:36 -0500

    udpates

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import './assets/css/style.css'
 
 export default {
   name: 'App',
@@ -18,11 +18,10 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Raleway', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 4px;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,41 +1,151 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="topnav">
+      <div class="top-left">
+        <a class="active" href="#home">Home</a> <!-- hide at top, display if others are clicked-->
+        <a href="#mission-statement">Mission Statement</a>
+        <a href="#gallery">Our Work</a>
+      </div>
+      <div class="top-mid">
+        <img class="mid-img" src="../assets/Logo.jpg" style="height: 60px;">
+      </div>
+      <div class="top-right">
+        <a href="#footer">About Us</a>
+      </div>
+    </div>
+
+
+    <vueper-slides id="home" fade :touchable="false" autoplay>
+    <vueper-slide
+      v-for="(slide, i) in slides"
+      :key="i"
+      :image="slide.image" />
+    </vueper-slides>
+
+
+    <!--mission statement -->
+    <div id="mission-statement" class="stm">
+    <div class="mission-stm-box"> 
+      <div class="mission-stm">
+      Mission Statemtent
+      </div>
+      <div class="mission-stm-desc">
+      Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. 
+      </div>
+    </div>
+    </div>
+
+
+    <div class="image-grid">
+      <div id="gallery" class="gallery">
+        <div class="gallery-panel"
+            v-for="photo in images"
+            :key="photo.id">
+          <img :src="photo.path">
+        </div>
+      </div>
+    </div>
+
+    <div id="footer" class="footer">
+      <div class="footer-logo">
+        <img src="../assets/logo-trans-white.png">
+      </div>
+      <div class="footer-info">
+
+        <div class="footer-contact">
+          <div>
+            <span>Contact us</span>
+          </div>
+        </div>
+
+        <div class="footer-clients">
+          <div>
+            <span> Our Clients</span>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
+import { VueperSlides, VueperSlide } from 'vueperslides'
+
+import 'vueperslides/dist/vueperslides.css'
+
 export default {
+  components: { VueperSlides, VueperSlide },
   name: 'HelloWorld',
+  methods: {
+    show () {
+            this.$modal.show('modal');
+        },
+        hide () {
+            this.$modal.hide('modal');
+        }, 
+        mount () {
+        this.show()
+    }
+  },
   props: {
     msg: String
-  }
+  },
+
+  data: () => ({
+  images: [
+    {
+      path: require('@/assets/image1.jpg'),
+      description: "some image",
+    },
+    {
+      path: require('@/assets/image2.jpg'),
+      description: "some image",
+    },
+    {
+      path: require('@/assets/image4.jpg'),
+      description: "some image",
+    },
+    {
+      path: require('@/assets/image5.jpg'),
+      description: "some image",
+    },
+    {
+      path: require('@/assets/image6.jpg'),
+      description: "some image",
+    },
+    {
+      path: require('@/assets/image7.jpg'),
+      description: "some image",
+    },
+    {
+      path: require('@/assets/image8.jpg'),
+      description: "some image",
+    },
+  ],
+  slides: [
+  {
+    title: 'El Teide Volcano, Spain',
+    content: 'Photo by Max Rive',
+    // You can also provide a URL for the image.
+    image: require('@/assets/image1.jpg')
+  },
+  {
+    title: 'El Teide Volcano, Spain',
+    content: 'Photo by Max Rive',
+    // You can also provide a URL for the image.
+    image: require('@/assets/image2.jpg')
+  },
+  {
+    title: 'El Teide Volcano, Spain',
+    content: 'Photo by Max Rive',
+    // You can also provide a URL for the image.
+    image: require('@/assets/image3.jpg')
+  },
+  // Other slides.
+  ]
+  }),
 }
 </script>
 
